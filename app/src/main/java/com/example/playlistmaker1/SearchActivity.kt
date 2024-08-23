992bc9ab7fd4181ca1338a3078100fd489f224c2
PaladinLeonid <PaladinPlay@yandex.ru>
2024-08-23 17:15:57 +0300

    Sprint 8 project

--- app/src/main/java/com/example/playlistmaker1/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker1/SearchActivity.kt
@@ -1,21 +1,64 @@
 package com.example.playlistmaker1
-
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.isVisible
+
 
 class SearchActivity : AppCompatActivity() {
+    private lateinit var searchField: EditText
+    private var searchText: String? = null
+
+    @SuppressLint("ServiceCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        searchField = findViewById(R.id.search_edit_field)
         val backButton = findViewById<ImageView>(R.id.arrow_back)
+        val clearIcon = findViewById<ImageView>(R.id.search_clear_btn)
         supportActionBar?.hide()
+
         backButton.setOnClickListener {
-            val mainsIntent = Intent(this, MainActivity::class.java)
-            startActivity(mainsIntent)
+            finish()
+        }
+        clearIcon.setOnClickListener {
+            searchField.setText("")
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(it.windowToken, 0)
+        }
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButtonVisibility(s, clearIcon)
+                searchText = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
 
         }
+        searchField.addTextChangedListener(simpleTextWatcher)
+    }
+
+
+    private fun clearButtonVisibility(s: CharSequence?, clearButton: ImageView) {
+        val clearIcon = findViewById<ImageView>(R.id.search_clear_btn)
+        clearIcon.isVisible = !s.isNullOrEmpty()
     }
 }
+
--- app/src/main/java/com/example/playlistmaker1/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker1/SettingsActivity.kt
@@ -1,22 +1,87 @@
 package com.example.playlistmaker1
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.widget.ImageView
+import android.widget.Switch
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 
 class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         val backButton = findViewById<ImageView>(R.id.arrow_back)
+        val forwardButtonImg = findViewById<ImageView>(R.id.forwardButtonImg)
+
+        val forwardButtonTxt = findViewById<TextView>(R.id.forwardButtonTxt)
+        val supportBtnImg = findViewById<ImageView>(R.id.supportBtnImg)
+
+        val supportBtnTxt = findViewById<TextView>(R.id.supportBtnTxt)
+        val settingsShareImg = findViewById<ImageView>(R.id.settings_share)
+
+        val settingsShareTxt = findViewById<TextView>(R.id.settingsShareTxt)
+
+        settingsShareImg.setOnClickListener { shareNameApp() }
+        settingsShareTxt.setOnClickListener { shareNameApp() }
+
+        supportBtnImg.setOnClickListener{supportEmail()}
+        supportBtnTxt.setOnClickListener{supportEmail()}
+
+        forwardButtonImg.setOnClickListener{offer()}
+        forwardButtonTxt.setOnClickListener{offer()}
+
         supportActionBar?.hide()
         backButton.setOnClickListener {
             val mainsIntent = Intent(this, MainActivity::class.java)
             startActivity(mainsIntent)
-        }
 
         }
 
     }
+
+    private fun offer() {
+        Intent().apply {
+            val url = getString(R.string.offerlink)
+            action = Intent.ACTION_VIEW
+            data = Uri.parse(url)
+            startActivity(this)
+        }
+    }
+
+
+
+    private fun supportEmail() {
+        val mail = "PaladinPlay@yandex.ru"
+        val title = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+        val body = "Спасибо разработчикам и разработчицам за крутое приложение!"
+        val action = Intent(Intent.ACTION_SENDTO).apply {
+        data = Uri.parse("mailto:$mail")
+        putExtra(Intent.EXTRA_SUBJECT, title)
+        putExtra(Intent.EXTRA_TEXT, body)
+    }
+        startActivity(action)
+    }
+
+    private fun shareNameApp() {
+      val urlLink = "https://practicum.yandex.ru/android-developer/"
+        val share = Intent().apply{
+            action = Intent.ACTION_SEND // Тип экшена
+            putExtra(Intent.EXTRA_TEXT, urlLink) // Текст сообщения
+            type = "text/plain" // Тип содержания
+            startActivity(Intent.createChooser(this, null));
+        }
+
+    }
+}
+
+       
+
+   
+
+
